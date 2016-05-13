b627c70a02e232f57472d85da570fa172d910455
Riza Fahmi <rizafahmi@gmail.com>
2016-05-13 18:04:15 +0700

    Finish components

--- src/index.js
+++ src/index.js
@@ -1,18 +1,45 @@
-import React from 'react'
+import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
+import YTSearch from 'youtube-api-search'
 
 import SearchBar from './components/search_bar'
+import VideoList from './components/video_list'
+import VideoDetail from './components/video_detail'
 
 const API_KEY = 'AIzaSyBLPlQ_6chNoF9kLmeuMRqD9mdSdSZ_CCc'
 
+
 // Create a new component
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  )
+class App extends Component {
+  constructor(prop) {
+    super(prop)
+
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    }
+
+    YTSearch({key: API_KEY, term: 'reactjs'}, (videos) => {
+      this.setState({
+        videos,
+        selectedVideo: videos[0]
+      })
+    })
+
+  }
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+          videos={this.state.videos}
+        />
+      </div>
+    )
+  }
 }
 
 
